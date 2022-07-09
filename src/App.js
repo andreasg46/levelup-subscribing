e0ae8733ac8f8e18ad92fbd989442b8e6d8f0e2a
ageorgiou46 <andreasg.silversky@gmail.com>
2022-07-09 18:22:14 +0300

    Updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React, { Component, Suspense } from 'react'
+import React, { useEffect, Component, Suspense } from 'react'
 import { HashRouter, Route, Routes } from 'react-router-dom'
 import './scss/style.scss'
 import OneSignal from 'react-onesignal';
-
 
 const loading = (
   <div className="pt-3 text-center">
@@ -13,23 +12,21 @@ const loading = (
 // Containers
 const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
 
-useEffect(() => {
-  OneSignal.init({ appId: 'cca1bd1f-8df1-47e4-a28e-454eeb0b5ab3', allowLocalhostAsSecureOrigin: true });
-  OneSignal.showSlidedownPrompt();
-}, []);
+const App = () => {
+  useEffect(() => {
+    OneSignal.init({ appId: 'cca1bd1f-8df1-47e4-a28e-454eeb0b5ab3', allowLocalhostAsSecureOrigin: true });
+    OneSignal.showSlidedownPrompt();
+  }, []);
 
-class App extends Component {
-  render() {
-    return (
-      <HashRouter>
-        <Suspense fallback={loading}>
-          <Routes>
-            <Route path="*" name="Home" element={<DefaultLayout />} />
-          </Routes>
-        </Suspense>
-      </HashRouter>
-    )
-  }
+  return (
+    <HashRouter>
+      <Suspense fallback={loading}>
+        <Routes>
+          <Route path="*" name="Home" element={<DefaultLayout />} />
+        </Routes>
+      </Suspense>
+    </HashRouter>
+  )
 }
 
 export default App
